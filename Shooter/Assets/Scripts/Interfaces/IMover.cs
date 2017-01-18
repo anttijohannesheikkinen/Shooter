04e637641428c8fd14483ff7191d16fe94ae14c6
Antti Heikkinen <antti.heikkinen@cs.tamk.fi>
2017-01-18 11:02:26 +0200

    Moving interface..

---
+++ Shooter/Assets/Scripts/Interfaces/IMover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Shooter
+{
+    public interface IMover
+    {
+        // Position of this object in world space.
+        Vector3 Position { get; set; }
+
+        // Rotation of this object in world space.
+        Quaternion Rotation { get; set; }
+
+        //The speed of this mover.
+        float Speed { get; }
+
+        // Moves toward targetPosition.
+        void MoveTowardsPosition(Vector3 targetPosition);
+        // Move to direction 'direction'.
+        void MoveToDirection(Vector3 direction);
+        //Rotate toward position 'targetPosition'.
+        void RotateTowardPosition(Vector3 targetPosition);
+    }
+}
--- Shooter/Assets/Scripts/Unit/UnitBase.cs
+++ Shooter/Assets/Scripts/Unit/UnitBase.cs
@@ -5,31 +5,16 @@ using UnityEngine;
 
 namespace Shooter
 {
-    public class UnitBase : MonoBehaviour
+    public abstract class UnitBase : MonoBehaviour
     {
 
         public static int UnitCounter { get; set; }
-
-        // Use this for initialization
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
 
         public virtual void Move()
         {
 
         }
 
-        public abstract void Shoot()
-        {
-
-        }
+        public abstract void Shoot();
     }
 }
